8b141e398ecb0e6f2f2f1ba69f7d4412602ec9f8
Emad Anwer <emadanwer.official@gmail.com>
2023-08-12 15:25:16 +0300

    t

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
-"""This is the file_storage."""
+#!usr/bin/python3
 
-from json import dump
-from json import load
+""" File storage model """
+import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -12,38 +11,46 @@ from models.place import Place
 from models.review import Review
 
 
-class FileStorage:
-    """File storage Class.
+class FileStorage():
 
-    serializes instances to a JSON file and deserializes
-    JSON file to instances.
     """
+    FileStorage class
+    Usage : implement the functionality of storing objects and retrieving them
+    Cicle : <class 'BaseModel'> -> to_dict() -> <class 'dict'> -> JSON dump ->
+    <class 'str'> ->FILE -> <class 'str'> -> JSON load -> <class 'dict'>
+    -> <class 'BaseModel'
+    """
+
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """All.
+    def __init__(self):
+        """do nothing"""
+        pass
 
-        Returns:
-            dictionary __objects
+    def all(self):
+        """
+        Returns all objects in BaseModel class representing format
         """
         return self.__objects
 
     def new(self, obj):
-        """Set in __objects the obj with key.
-
-        Args:
-            obj (object): object
+        """
+        Add new created objects to __objects var to serialize them
+        into json objects using save() later
         """
         self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file."""
+        """"
+        Serialize BaseModel objects in __objects to json objects and
+        save them in file.json file format
+        """
         json_objs = {}
         for key, val in self.__objects.items():
             json_objs[key] = val.to_dict()
-        with open(self.__file_path, "w+") as f:
-            dump(json_objs, f)
+        with open(self.__file_path, "w", encoding='utf-8') as f:
+            json.dump(json_objs, f, indent=4)
 
     def reload(self):
         """
