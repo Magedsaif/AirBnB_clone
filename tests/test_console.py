0bd76ccabcf2e20a3fc7841f0fda3b86fa579ebf
Emad Anwer <emadanwer.official@gmail.com>
2023-08-13 13:44:38 +0300

    test create

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,22 +17,24 @@ class TestConstructor(unittest.TestCase):
             HBNBCommand().onecmd("help quit")
             self.assertIn("Quit command to exit the program.", f.getvalue())
 
+            HBNBCommand().onecmd("help EOF")
+            self.assertIn("EOF command to exit the program.", f.getvalue())
 
-    # def test_create(self):
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         HBNBCommand().onecmd("create")
-    #         self.assertEqual("** class name missing **", f.getvalue()[:-1])
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         HBNBCommand().onecmd("create Emad")
-    #         self.assertEqual("** class doesn't exist **", f.getvalue()[:-1])
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         HBNBCommand().onecmd("create Emad")
-    #         self.assertEqual("** class doesn't exist **", f.getvalue()[:-1])
+    def test_create(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create")
+            self.assertEqual("** class name missing **", f.getvalue()[:-1])
+        # with patch('sys.stdout', new=StringIO()) as f:
+        #     HBNBCommand().onecmd("create Emad")
+        #     self.assertEqual("** class doesn't exist **", f.getvalue()[:-1])
+        # with patch('sys.stdout', new=StringIO()) as f:
+        #     HBNBCommand().onecmd("create Emad")
+        #     self.assertEqual("** class doesn't exist **", f.getvalue()[:-1])
 
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         HBNBCommand().onecmd("create User")
-    #         classes_dict = storage.all()
-    #         self.assertTrue("User."+f.getvalue()[:-1] in storage.all().keys())
+        # with patch('sys.stdout', new=StringIO()) as f:
+        #     HBNBCommand().onecmd("create User")
+        #     classes_dict = storage.all()
+        #     self.assertTrue("User."+f.getvalue()[:-1] in storage.all().keys())
 
     # def test_show(self):
     #     with patch('sys.stdout', new=StringIO()) as f:
