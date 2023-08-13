ba220304075e788978f0b4f6739c996cdc985f0d
Emad Anwer <emadanwer.official@gmail.com>
2023-08-13 11:41:16 +0300

    test 7

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -45,7 +45,6 @@ class TestConstructor(unittest.TestCase):
         # Verify that the attributes of the added object match the original attributes
         reloaded_obj = self.fs.all()[obj_key]
         self.assertEqual(reloaded_obj.id, new_base_model.id)
-        self.assertEqual(reloaded_obj.created_at, new_base_model.created_at)
-        self.assertEqual(reloaded_obj.updated_at, new_base_model.updated_at)
+    
         
         
