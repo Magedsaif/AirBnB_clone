32ae3b1e154bed0509323ee911c2a4f0ee99b50d
Emad Anwer <emadanwer.official@gmail.com>
2023-08-13 12:55:45 +0300

    test 2

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,9 +6,12 @@ import unittest
 from console import HBNBCommand
 from unittest.mock import patch
 from io import StringIO
+from models.user import User
 
 
 class TestConstructor(unittest.TestCase):
+
+
     def test_help_method(self):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("help quit")
@@ -36,3 +39,78 @@ class TestConstructor(unittest.TestCase):
             HBNBCommand().onecmd("help update")
             self.assertIn("""Updates an instance based on the class name and id by adding
         or updating attribute (save the change into the JSON file).""", f.getvalue())
+
+    def test_create(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create")
+            self.assertIn("** class name missing **", f.getvalue())
+
+            HBNBCommand().onecmd("create Emad")
+            self.assertIn("** class doesn't exist **", f.getvalue())
+
+            # TODO create class
+
+    def test_show(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show")
+            self.assertIn("** class name missing **", f.getvalue())
+
+            HBNBCommand().onecmd("show Emad")
+            self.assertIn("** class doesn't exist **", f.getvalue())
+
+            HBNBCommand().onecmd("show User")
+            self.assertIn("** instance id missing **", f.getvalue())
+
+            HBNBCommand().onecmd("show User 3212133")
+            self.assertIn("** no instance found **", f.getvalue())
+            # TODO show class
+
+    def test_destroy(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy")
+            self.assertIn("** class name missing **", f.getvalue())
+
+            HBNBCommand().onecmd("destroy Emad")
+            self.assertIn("** class doesn't exist **", f.getvalue())
+
+            HBNBCommand().onecmd("destroy User")
+            self.assertIn("** instance id missing **", f.getvalue())
+
+            HBNBCommand().onecmd("destroy User 3212133")
+            self.assertIn("** no instance found **", f.getvalue())
+            # TODO destroy class
+
+    def test_all(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("all Emad")
+            self.assertIn("** class doesn't exist **", f.getvalue())
+
+            # TODO all class
+
+    def test_update(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("update")
+            self.assertIn("** class name missing **", f.getvalue())
+
+            HBNBCommand().onecmd("update Emad")
+            self.assertIn("** class doesn't exist **", f.getvalue())
+
+            HBNBCommand().onecmd("update User")
+            self.assertIn("** instance id missing **", f.getvalue())
+
+            HBNBCommand().onecmd("update User 3212133")
+            self.assertIn("** no instance found **", f.getvalue())
+            # create a new user
+            new_user = User()
+            
+            new_user.save()
+            HBNBCommand().onecmd(f"update User {new_user.id}")
+            self.assertIn("** attribute name missing **", f.getvalue())
+
+
+            HBNBCommand().onecmd(f"update User {new_user.id} name")
+            self.assertIn("** value missing **", f.getvalue())
+
+            HBNBCommand().onecmd(f"update User {new_user.id} name 'emad'")
+            self.assertIn("", f.getvalue())
+            # TODO destroy class
