ed05529d6c26355efeeb771e29884a20d73720ca
Emad Anwer <emadanwer.official@gmail.com>
2023-08-13 13:41:36 +0300

    test help

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,28 +17,6 @@ class TestConstructor(unittest.TestCase):
             HBNBCommand().onecmd("help quit")
             self.assertIn("Quit command to exit the program.", f.getvalue())
 
-            HBNBCommand().onecmd("help EOF")
-            self.assertIn("EOF command to exit the program.", f.getvalue())
-
-            HBNBCommand().onecmd("help create")
-            self.assertIn("""Creates a new instance of BaseModel, saves it
-        (to the JSON file) and prints the id""", f.getvalue())
-
-            HBNBCommand().onecmd("help show")
-            self.assertIn("""Print the string representation of an instance
-        based on the class name and id.""", f.getvalue())
-
-            HBNBCommand().onecmd("help destroy")
-            self.assertIn("""Delete an instance based on the class name and id.
-        (save the change into the JSON file).""", f.getvalue())
-
-            HBNBCommand().onecmd("help all")
-            self.assertIn("""Print all string representation of all instances
-        based or not on the class name.""", f.getvalue())
-
-            HBNBCommand().onecmd("help update")
-            self.assertIn("""Updates an instance based on the class name and id by adding
-        or updating attribute (save the change into the JSON file).""", f.getvalue())
 
     def test_create(self):
         with patch('sys.stdout', new=StringIO()) as f:
