dd52ada760819d15a9f959ba6807153b01678b13
Emad Anwer <emadanwer.official@gmail.com>
2023-08-13 13:26:23 +0300

    test 1

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,10 +7,9 @@ from console import HBNBCommand
 from unittest.mock import patch
 from io import StringIO
 from models.user import User
-
+from models import storage
 
 class TestConstructor(unittest.TestCase):
-
 
     def test_help_method(self):
         with patch('sys.stdout', new=StringIO()) as f:
@@ -43,12 +42,18 @@ class TestConstructor(unittest.TestCase):
     def test_create(self):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create")
-            self.assertIn("** class name missing **", f.getvalue())
-
+            self.assertEqual("** class name missing **", f.getvalue()[:-1])
+        with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create Emad")
-            self.assertIn("** class doesn't exist **", f.getvalue())
+            self.assertEqual("** class doesn't exist **", f.getvalue()[:-1])
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create Emad")
+            self.assertEqual("** class doesn't exist **", f.getvalue()[:-1])
 
-            # TODO create class
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create User")
+            classes_dict = storage.all()
+            self.assertTrue("User."+f.getvalue()[:-1] in storage.all().keys())
 
     def test_show(self):
         with patch('sys.stdout', new=StringIO()) as f:
@@ -102,11 +107,10 @@ class TestConstructor(unittest.TestCase):
             self.assertIn("** no instance found **", f.getvalue())
             # create a new user
             new_user = User()
-            
+
             new_user.save()
             HBNBCommand().onecmd(f"update User {new_user.id}")
             self.assertIn("** attribute name missing **", f.getvalue())
-
 
             HBNBCommand().onecmd(f"update User {new_user.id} name")
             self.assertIn("** value missing **", f.getvalue())
